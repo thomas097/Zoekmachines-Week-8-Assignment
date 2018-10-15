698223641b6170902e3eebe0a611a3b95c4ccc73
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-10-15 12:52:43 +0200

    Add files via upload

--- simple_search.py
+++ simple_search.py
@@ -27,8 +27,8 @@ def create_snippet(lyrics, query, length=20):
 
 # implements simple keyword search in the indexed lyrics
 # returns N tuples of (title, artist, genre, year, lyrics snippet)
-def simple_search(es, index, _type, query, N=10):
-    res = es.search(index=index, body={"query": {"match": {'lyrics': query}}})
+def simple_search(es, index, query, N=10):
+    res = es.search(index=index, body={"query": {"match": {'lyrics': query}}, "size":N})
     results_list = []
     for hit in res['hits']['hits']:
         song = hit['_source']
@@ -40,6 +40,6 @@ def simple_search(es, index, _type, query, N=10):
 '''
 # init elastic search
 es = Elasticsearch(hosts=['http://localhost:9200/'])
-res = simple_search(es, 'songs', 'song', 'all the single ladies')
+res = simple_search(es, 'songs', 'all the single ladies', N=20)
 pprint(res)
 '''
