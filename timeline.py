5437eed24cff92eddc400139533f058e1bab21f6
ThBellucci <40698255+thomas097@users.noreply.github.com>
2018-10-21 23:13:42 +0200

    nathan's code terug gezet (+ nieuwe versie oude)

--- timeline.py
+++ timeline.py
@@ -1,8 +1,14 @@
 from elasticsearch import Elasticsearch, helpers
 from collections import Counter
 from pprint import pprint
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import string
+from random import *
+import os
 
 '''
     Usage:
@@ -10,6 +16,36 @@ import numpy as np
         2) run function timeline()
         3) (optional) call from demo
 '''
+
+# removes all images in images to prevent overdosis of images
+def clear_images():
+    folder = './static/images/'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)
+
+def new_timeline(res):
+    clear_images()
+    # determine number of documents (that match the query) with a particular year
+    counts = Counter([int(x['_source']['year']) for x in res['hits']['hits']])
+    years, counts = zip(*sorted(counts.items()))
+
+    # create figure and save to path
+    fig, ax = plt.subplots(figsize=(10, 2))
+    # ax.set_yticklabels([])
+    # plt.plot(years, counts, 'b-')
+    ax.bar(years, counts, .5, color='#a80000')
+
+    # prevent using cached images over and over again
+    random_string = "".join(choice(string.ascii_letters) for x in range(5))
+
+    fig.savefig('./static/images/timeline' + str(random_string) + '.png')
+    return str(random_string)
+
 
 
 # implements the timeline feature by resubmitting a query, with
@@ -49,6 +85,6 @@ def timeline(es, index, lyrics, song_title, artist, path):
 
 
 # init elastic search
-es = Elasticsearch(hosts=['http://localhost:9200/'])
-timeline(es, 'songs', '', '', 'james bay', 'timeline.png')
+#es = Elasticsearch(hosts=['http://localhost:9200/'])
+#timeline(es, 'songs', '', '', 'james bay', 'timeline.png')
 
